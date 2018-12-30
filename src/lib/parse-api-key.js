dbd1d62c82c840578121ed1580b139ecb157b65f
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2018-12-31 00:05:26 +0900

    Remove host detect to parse api key

--- src/lib/parse-api-key.js
+++ src/lib/parse-api-key.js
@@ -1,20 +1,12 @@
 import urlParse from 'url-parse'
 import qs from 'querystring'
 
-const allowedHosts = [
-  '127.0.0.1',
-  'localhost',
-  'tilecloud.io',
-  'github.tilecloud.io',
-  'api.tilecloud.io',
-]
-
 export default () => {
   const scripts = document.getElementsByTagName('script')
   for (const script of scripts) {
-    const { hostname, query } = urlParse(script.src)
+    const { query } = urlParse(script.src)
     const { apiKey, tilecloud } = qs.parse(query.replace(/^\?/, ''))
-    if (allowedHosts.includes(hostname) && tilecloud === 'true') {
+    if (tilecloud === 'true') {
       return apiKey
     }
   }
