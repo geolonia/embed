b72203ec6e5d6fda29e6fb1d0958c8adf32e2eb9
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-04-05 18:56:47 +0900

    Rename: @tilecloud/js->@tilecloud/embed

--- sample/webpack.config.js
+++ sample/webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  entry: './src/tilecloud.js',
+  entry: './src/embed.js',
   output: {
     path: __dirname,
-    filename: 'tilecloud.js',
+    filename: 'embed',
   },
   devtool: 'inline-source-map',
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/tilecloud.js',
+  entry: './src/embed.js',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'tilecloud.js',
+    filename: 'embed.js',
   },
 
   module: {
