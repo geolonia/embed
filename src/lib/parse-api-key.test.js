5d4604efa481ce6fbee23c6ec6086664c1fbe683
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-04-12 04:06:07 +0900

    bug fix to parse api key

--- src/lib/parse-api-key.test.js
+++ src/lib/parse-api-key.test.js
@@ -2,13 +2,13 @@ import parseApiKey from './parse-api-key'
 import { JSDOM } from 'jsdom'
 import assert from 'assert'
 
-describe('not parse api key from dom', () => {
-  it('should not parse with tilecloud flag', () => {
+describe('parse api key from dom', () => {
+  it('should parse with tilecloud flag', () => {
     const { document: mocDocument } = new JSDOM(`<html><body>
-      <script src="https://external.example.com/tilecloud.js?key=abc"></script>
+      <script src="https://external.example.com/?tilecloud-api-key=abc"></script>
     </body></html>`).window
 
     const apiKey = parseApiKey(mocDocument)
-    assert.equal(apiKey, void 0)
+    assert.deepEqual('abc', apiKey)
   })
 })
