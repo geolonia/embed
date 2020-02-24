2cce2aa622eebfa188695c316c3018ba52b6e31b
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-02-25 04:48:14 +0900

    access denined from iframe

--- src/embed.js
+++ src/embed.js
@@ -8,48 +8,52 @@ import 'mapbox-gl/dist/mapbox-gl.css'
 import './style.css'
 import GeoloniaMap from './lib/geolonia-map'
 
-let isDOMContentLoaded = false
-const alreadyRenderedMaps = []
-const plugins = []
+if ( window === window.parent ) {
+  let isDOMContentLoaded = false
+  const alreadyRenderedMaps = []
+  const plugins = []
 
-document.addEventListener('DOMContentLoaded', () => {
-  isDOMContentLoaded = true
-  alreadyRenderedMaps.forEach(({ map, target }) =>
-    plugins.forEach(plugin => plugin(map, target)),
-  )
-  // clear
-  alreadyRenderedMaps.splice(0, alreadyRenderedMaps.length)
-})
-
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(item => {
-    if (!item.isIntersecting) {
-      return
-    }
-    const map = new GeoloniaMap(item.target)
-    if (isDOMContentLoaded) {
-      plugins.forEach(plugin => plugin(map, item.target))
-    } else {
-      alreadyRenderedMaps.push({ map, target: item.target })
-    }
-    observer.unobserve(item.target)
+  document.addEventListener('DOMContentLoaded', () => {
+    isDOMContentLoaded = true
+    alreadyRenderedMaps.forEach(({ map, target }) =>
+      plugins.forEach(plugin => plugin(map, target)),
+    )
+    // clear
+    alreadyRenderedMaps.splice(0, alreadyRenderedMaps.length)
   })
-})
 
-const containers = document.querySelectorAll('.tilecloud, .geolonia')
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(item => {
+      if (!item.isIntersecting) {
+        return
+      }
+      const map = new GeoloniaMap(item.target)
+      if (isDOMContentLoaded) {
+        plugins.forEach(plugin => plugin(map, item.target))
+      } else {
+        alreadyRenderedMaps.push({ map, target: item.target })
+      }
+      observer.unobserve(item.target)
+    })
+  })
 
-for (let i = 0; i < containers.length; i++) {
-  observer.observe(containers[i])
+  const containers = document.querySelectorAll('.tilecloud, .geolonia')
+
+  for (let i = 0; i < containers.length; i++) {
+    observer.observe(containers[i])
+  }
+
+  window.geolonia = mapboxgl
+  window.geolonia.Map = GeoloniaMap
+  window.geolonia.registerPlugin = plugin => {
+    plugins.push(plugin)
+    return void 0
+  }
+
+  // for backward compatibility
+  window.tilecloud = window.geolonia
+
+  window.mapboxgl = mapboxgl
+} else {
+  console.error( '[Geolonia] We are very sorry, but we can\'t display our map in iframe.' ) // eslint-disbale-line
 }
-
-window.geolonia = mapboxgl
-window.geolonia.Map = GeoloniaMap
-window.geolonia.registerPlugin = plugin => {
-  plugins.push(plugin)
-  return void 0
-}
-
-// for backward compatibility
-window.tilecloud = window.geolonia
-
-window.mapboxgl = mapboxgl
