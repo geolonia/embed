221e441c61b06346874b1a3ffac34b4828e7f4d4
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-03-03 18:57:13 +0900

    allow iframe on same origin

--- src/embed.js
+++ src/embed.js
@@ -8,7 +8,7 @@ import 'mapbox-gl/dist/mapbox-gl.css'
 import './style.css'
 import GeoloniaMap from './lib/geolonia-map'
 
-if ( window === window.parent ) {
+if ( window.location.origin === window.parent.location.origin ) {
   let isDOMContentLoaded = false
   const alreadyRenderedMaps = []
   const plugins = []
