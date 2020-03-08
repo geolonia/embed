d221de8f67a3d5b14b1fa6487a6e5ac4b5823aa6
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-03-08 17:38:39 +0900

    handle options

--- src/lib/geolonia-marker.js
+++ src/lib/geolonia-marker.js
@@ -1,4 +1,6 @@
 import mapboxgl from 'mapbox-gl'
+import markerSVG from 'svg-inline-loader!./marker.svg'
+
 
 /**
  * Render the map
@@ -6,9 +8,32 @@ import mapboxgl from 'mapbox-gl'
  * @param container
  */
 export default class GeoloniaMarker extends mapboxgl.Marker {
-  constructor(options) {
+  constructor(options, legacyOptions) {
+    if (!options || !options.element) {
+      const markerElement = document.createElement('div')
+      markerElement.className = 'geolonia-default-marker'
+      markerElement.innerHTML = markerSVG
 
-    super(options)
+      // The dimention of marker.svg is 52:67.
+      markerElement.style.margin = 0
+      markerElement.style.padding = 0
+      markerElement.style.width = '26px'
+      markerElement.style.height = '33.5px'
+
+      const markerObj = markerElement.querySelector('svg')
+      markerObj.style.width = '100%'
+      markerObj.style.height = '100%'
+
+      if (! options) {
+        options = {
+          element: markerElement
+        }
+      } else {
+        options.element = markerElement
+      }
+    }
+
+    super(options, legacyOptions)
     const marker = this
 
     return marker
