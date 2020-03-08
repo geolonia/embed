3697def549ce02a0be316558a6ac4b79e89390a8
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-03-08 20:46:42 +0900

    change default marker

--- src/lib/geolonia-marker.js
+++ src/lib/geolonia-marker.js
@@ -1,6 +1,9 @@
-import mapboxgl from 'mapbox-gl'
-import markerSVG from 'svg-inline-loader!./marker.svg'
+'use strict'
 
+import mapboxgl from 'mapbox-gl'
+import tinycolor from 'tinycolor2'
+import markerSVG from 'svg-inline-loader!./marker.svg'
+import * as util from './util'
 
 /**
  * Render the map
@@ -8,8 +11,17 @@ import markerSVG from 'svg-inline-loader!./marker.svg'
  * @param container
  */
 export default class GeoloniaMarker extends mapboxgl.Marker {
-  constructor(options, legacyOptions) {
-    if (!options || !options.element) {
+  constructor(options, legacyOptions = {}) {
+    if (options && util.isDomElement(options)) {
+      options = {
+        element: options,
+        ...legacyOptions,
+      }
+    } else if (!options) {
+      options = {}
+    }
+
+    if (!options.element) {
       const markerElement = document.createElement('div')
       markerElement.className = 'geolonia-default-marker'
       markerElement.innerHTML = markerSVG
@@ -17,23 +29,28 @@ export default class GeoloniaMarker extends mapboxgl.Marker {
       // The dimention of marker.svg is 52:67.
       markerElement.style.margin = 0
       markerElement.style.padding = 0
-      markerElement.style.width = '26px'
-      markerElement.style.height = '33.5px'
+      markerElement.style.width = '25.5px'
+      markerElement.style.height = '34.5px'
 
       const markerObj = markerElement.querySelector('svg')
       markerObj.style.width = '100%'
       markerObj.style.height = '100%'
 
-      if (! options) {
-        options = {
-          element: markerElement
-        }
+      options.element = markerElement
+
+      if (options.color) {
+        const color = tinycolor(options.color).darken().toString()
+        markerElement.querySelector('.left').style.fill = options.color
+        markerElement.querySelector('.right').style.fill = color
       } else {
-        options.element = markerElement
+        markerElement.querySelector('.left').style.fill = '#EE730F'
+        markerElement.querySelector('.right').style.fill = '#E84130'
       }
+
+      options.offset = [0, -17.25]
     }
 
-    super(options, legacyOptions)
+    super(options)
     const marker = this
 
     return marker
