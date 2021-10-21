ebb82f5573c2cfd00e3cb0e5a1c396031096a191
kamataryo <kamataryo@users.noreply.github.com>
2021-10-21 14:42:32 +0900

    Update webpack

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -1,19 +1,18 @@
+const path = require('path')
 const config = require('../webpack.config')
 
 module.exports = {
   ...config,
   output: {
+    ...config.output,
     path: __dirname,
-    filename: 'embed.js',
   },
   devtool: 'inline-source-map',
   devServer: {
+    static: {
+      directory: __dirname
+    },
     open: true,
-    openPage: 'index.html',
-    contentBase: __dirname,
-    watchContentBase: true,
-    host: 'localhost',
     port: 3000,
-    disableHostCheck: true
   },
 }
--- docs/webpack.deck-config.js
+++ docs/webpack.deck-config.js
@@ -4,7 +4,6 @@ module.exports = {
   ...config,
   devServer: {
     ...config.devServer,
-    openPage: 'deck.html',
+    open: 'deck.html',
   },
 }
-
