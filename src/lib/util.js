60c8dcf4d5f3fb59ae7cbc79b6b07ddbb3420b93
Junya Ishihara <junya@champierre.com>
2022-12-19 19:09:45 +0900

    simplestyleのポップアップ内にスタイルを適用したいときがあるので、class をエスケープしない

--- src/lib/util.js
+++ src/lib/util.js
@@ -279,5 +279,6 @@ export const sanitizeDescription = async (description) => {
   const { default: sanitizeHtml } = await import('sanitize-html');
   return sanitizeHtml(description, {
     allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'img' ]),
+    allowedAttributes: {...sanitizeHtml.defaults.allowedAttributes, '*': ['class']},
   });
 };
