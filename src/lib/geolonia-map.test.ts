02d8a38b8153d91ce87bd45384e846743316f5f0
naoki <n.globe.us@gmail.com>
2024-09-30 14:25:26 +0900

    some fix

--- src/lib/geolonia-map.test.ts
+++ src/lib/geolonia-map.test.ts
@@ -1,6 +1,6 @@
 
 window.URL.createObjectURL ||= (_: Blob | MediaSource) => 'dummy'; // To prevent `TypeError: window.URL.createObjectURL is not a function`
-import GeoloniaMap from './geolonia-map'; // GeoloniaMap のインポート
+import GeoloniaMap from './geolonia-map';
 
 
 describe('GeoloniaMap.loadImage', () => {
@@ -10,7 +10,6 @@ describe('GeoloniaMap.loadImage', () => {
 
 
   afterEach(() => {
-    // sinon.restore();
   });
 
   it('should call super.loadImage and resolve with expected image data', async () => {
@@ -21,18 +20,4 @@ describe('GeoloniaMap.loadImage', () => {
     });
 
   });
-
-
-  // it('should call callback with image data if callback is provided', (done) => {
-  //   const url = 'https://example.com/image.png';
-  //   const callback = sinon.spy((error, image, expiry) => {
-  //     expect(error).to.be.null;
-  //     expect(image).to.equal('mockedImageData');
-  //     expect(expiry.cacheControl).to.equal('mockedCacheControl');
-  //     expect(expiry.expires).to.equal('mockedExpires');
-  //     done();
-  //   });
-
-  //   geoloniaMap.loadImage(url, callback);
-  // });
 });
