ea0fbf9f5e0ce2aa311336364ed156a42f9acf9c
Yasuaki KAWAKAMI <yasuaki.kawakami@geolonia.com>
2025-09-01 13:56:33 +0900

    add unit test of layer data attributes

--- src/lib/geolonia-map.test.ts
+++ src/lib/geolonia-map.test.ts
@@ -32,28 +32,49 @@ const maplibregl = {
     remove: sinon.stub().returnsThis(),
   }),
 };
+const SimpleStyle = sinon.stub().returns({
+  addTo: sinon.stub().returnsThis(),
+});
+const SimpleStyleVector = sinon.stub().returns({
+  addTo: sinon.stub().returnsThis(),
+});
 
-// require.cache を使って モックを注入
-require.cache[require.resolve('maplibre-gl')] = {
-  exports: maplibregl,
-} as any;
-require.cache[require.resolve('./geolonia-marker')] = {
-  exports: maplibregl.Marker,
-} as any;
-
-import GeoloniaMap from './geolonia-map';
 import {keyring} from './keyring';
 
 describe('GeoloniaMap constructor options', () => {
   let dom: JSDOM;
   let mapStub: sinon.SinonStub;
   let markerStub: sinon.SinonStub;
+  let simpleStyleStub: sinon.SinonStub;
+  let simpleStyleVectorStub: sinon.SinonStub;
+  let GeoloniaMap: any;
+
+  let originalRequireCache: any;
+
+  before(async () => {
+    // 元のcacheを保存
+    originalRequireCache = {
+      'maplibre-gl': require.cache[require.resolve('maplibre-gl')],
+      './geolonia-marker': require.cache[require.resolve('./geolonia-marker')],
+      './simplestyle': require.cache[require.resolve('./simplestyle')],
+      './simplestyle-vector': require.cache[require.resolve('./simplestyle-vector')],
+    };
+
+    // モックを設定
+    require.cache[require.resolve('maplibre-gl')] = {exports: maplibregl} as any;
+    require.cache[require.resolve('./geolonia-marker')] = {exports: maplibregl.Marker} as any;
+    require.cache[require.resolve('./simplestyle')] = {exports: {SimpleStyle}} as any;
+    require.cache[require.resolve('./simplestyle-vector')] = {exports: SimpleStyleVector} as any;
+
+    GeoloniaMap = (await import('./geolonia-map')).default;
+  });
 
   beforeEach(() => {
+    sinon.resetHistory();
     mapStub = maplibregl.Map as sinon.SinonStub;
     markerStub = maplibregl.Marker as sinon.SinonStub;
-    mapStub.resetHistory();
-    markerStub.resetHistory();
+    simpleStyleStub = SimpleStyle as sinon.SinonStub;
+    simpleStyleVectorStub = SimpleStyleVector as sinon.SinonStub;
 
     // DOM環境をセットアップ
     dom = new JSDOM('<!DOCTYPE html><html><body></body></html>');
@@ -67,6 +88,18 @@ describe('GeoloniaMap constructor options', () => {
 
   afterEach(() => {
     keyring.reset();
+  });
+
+  after(() => {
+    // 元のcacheを復元
+    Object.keys(originalRequireCache).forEach((key) => {
+      if (originalRequireCache[key]) {
+        require.cache[require.resolve(key)] = originalRequireCache[key];
+      } else {
+        delete require.cache[require.resolve(key)];
+      }
+    });
+    sinon.restore();
   });
 
   it('should pass basic position attributes to MapLibre constructor', () => {
@@ -182,5 +215,144 @@ describe('GeoloniaMap constructor options', () => {
     assert(markerOptions['element'], 'Marker options should include custom marker element');
     assert(markerOptions['element'].id === 'custom-marker');
     assert.deepEqual(markerOptions['offset'], [0, -25], 'Marker options should include offset from data-custom-marker-offset');
+  });
+
+  it('should pass GeoJSON with SimpleStyle', () => {
+    const geoJSON = {
+      'type': 'FeatureCollection',
+      'features': [
+        {
+          'type': 'Feature',
+          'geometry': {
+            'type': 'Point',
+            'coordinates': [
+              139.767125,
+              35.681236,
+            ],
+          },
+          'properties': {
+            'title': 'Tokyo Station',
+            'marker-size': 'large',
+            'marker-color': '#ff0000',
+            'marker-symbol': 'rail-metro',
+          },
+        },
+      ],
+    };
+    dom.window.document.body.innerHTML = `
+      <script type="application/json" id="example-geojson">${JSON.stringify(geoJSON)}</script>
+      <div>
+        <div class="geolonia"
+            data-geojson="#example-geojson"
+            data-marker="off"
+        ></div>
+      </div>
+    `;
+
+    const container = dom.window.document.querySelector('.geolonia') as HTMLElement;
+    new GeoloniaMap({container});
+    const styledataCallback = mockMapInstance.on.getCalls()
+      .find((call) => call.args[0] === 'styledata')?.args[1];
+    assert(styledataCallback, 'Map load event listener should be registered');
+    styledataCallback({target: mockMapInstance}); // 'load' イベントを手動でトリガー
+
+    assert(simpleStyleStub.calledOnce, 'SimpleStyle constructor should be called once');
+    const [geojsonArg] = simpleStyleStub.getCall(0).args;
+    assert.deepEqual(geojsonArg, geoJSON, 'GeoJSON passed to SimpleStyle should match the input');
+  });
+
+  it('should pass GeoJSON URL with SimpleStyle', () => {
+    const url = 'https://embed.geolonia.test.com/data.geojson';
+    dom.window.document.body.innerHTML = `
+      <div>
+        <div class="geolonia"
+            data-geojson="${url}"
+            data-marker="off"
+        ></div>
+      </div>
+    `;
+
+    const container = dom.window.document.querySelector('.geolonia') as HTMLElement;
+    new GeoloniaMap({container});
+    const styledataCallback = mockMapInstance.on.getCalls()
+      .find((call) => call.args[0] === 'styledata')?.args[1];
+    assert(styledataCallback, 'Map load event listener should be registered');
+    styledataCallback({target: mockMapInstance}); // 'load' イベントを手動でトリガー
+
+    assert(simpleStyleStub.calledOnce, 'SimpleStyle constructor should be called once');
+    const [geojsonArg] = simpleStyleStub.getCall(0).args;
+    assert.deepEqual(geojsonArg, url, 'GeoJSON passed to SimpleStyle should match the input');
+  });
+
+  it('should pass simple-vector with SimpleStyle', () => {
+    const url = 'https://embed.geolonia.test.com/embed-simple-vector-sample/tiles.json';
+    dom.window.document.body.innerHTML = `
+      <div>
+        <div class="geolonia"
+            data-simple-vector="${url}"
+            data-marker="off"
+        ></div>
+      </div>
+    `;
+
+    const container = dom.window.document.querySelector('.geolonia') as HTMLElement;
+    new GeoloniaMap({container});
+    const styledataCallback = mockMapInstance.on.getCalls()
+      .find((call) => call.args[0] === 'styledata')?.args[1];
+    assert(styledataCallback, 'Map load event listener should be registered');
+    styledataCallback({target: mockMapInstance}); // 'load' イベントを手動でトリガー
+
+    assert(simpleStyleVectorStub.calledOnce, 'SimpleStyleVector constructor should be called once');
+    const [urlArg] = simpleStyleVectorStub.getCall(0).args;
+    assert.deepEqual(urlArg, url, 'url passed to SimpleStyleVector should match the input');
+  });
+
+  it('should pass simple-vector with geolonia:// schema', () => {
+    const url = 'geolonia://embed-simple-vector-sample/tiles.json';
+    dom.window.document.body.innerHTML = `
+      <div>
+        <div class="geolonia"
+            data-simple-vector="${url}"
+            data-marker="off"
+        ></div>
+      </div>
+    `;
+
+    const container = dom.window.document.querySelector('.geolonia') as HTMLElement;
+    new GeoloniaMap({container});
+    const styledataCallback = mockMapInstance.on.getCalls()
+      .find((call) => call.args[0] === 'styledata')?.args[1];
+    assert(styledataCallback, 'Map load event listener should be registered');
+    styledataCallback({target: mockMapInstance}); // 'load' イベントを手動でトリガー
+
+    assert(simpleStyleVectorStub.calledOnce, 'SimpleStyleVector constructor should be called once');
+    const [urlArg] = simpleStyleVectorStub.getCall(0).args;
+    assert.deepEqual(urlArg, url, 'url passed to SimpleStyleVector should match the input');
+  });
+
+  it('should pass simple-vector with tileId', () => {
+    const tileId = '2743bbe4da254ab2b6a6477129203d60';
+    dom.window.document.body.innerHTML = `
+      <div>
+        <div class="geolonia"
+            data-simple-vector="${tileId}"
+            data-marker="off"
+        ></div>
+      </div>
+    `;
+
+    const container = dom.window.document.querySelector('.geolonia') as HTMLElement;
+    new GeoloniaMap({container});
+    const styledataCallback = mockMapInstance.on.getCalls()
+      .find((call) => call.args[0] === 'styledata')?.args[1];
+    assert(styledataCallback, 'Map load event listener should be registered');
+    styledataCallback({target: mockMapInstance}); // 'load' イベントを手動でトリガー
+
+    assert(simpleStyleVectorStub.calledOnce, 'SimpleStyleVector constructor should be called once');
+    const [urlArg] = simpleStyleVectorStub.getCall(0).args;
+    assert.deepEqual(urlArg,
+      `geolonia://tiles/custom/${tileId}`,
+      'url passed to SimpleStyleVector should match the input with geolonia schema',
+    );
   });
 });
