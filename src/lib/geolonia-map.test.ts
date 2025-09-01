dabb36eb2db1165f6aa9b51bddfdb6314e43927e
Yasuaki KAWAKAMI <yasuaki.kawakami@geolonia.com>
2025-09-01 10:59:16 +0900

    add unit tests for marker attributes

--- src/lib/geolonia-map.test.ts
+++ src/lib/geolonia-map.test.ts
@@ -22,6 +22,14 @@ const maplibregl = {
     setLngLat: sinon.stub().returnsThis(),
     addTo: sinon.stub().returnsThis(),
     remove: sinon.stub().returnsThis(),
+    setPopup: sinon.stub().returnsThis(),
+    getElement: sinon.stub().returns(document.createElement('div')),
+  }),
+  Popup: sinon.stub().returns({
+    setLngLat: sinon.stub().returnsThis(),
+    setHTML: sinon.stub().returnsThis(),
+    addTo: sinon.stub().returnsThis(),
+    remove: sinon.stub().returnsThis(),
   }),
 };
 
@@ -49,6 +57,8 @@ describe('GeoloniaMap constructor options', () => {
 
     // DOM環境をセットアップ
     dom = new JSDOM('<!DOCTYPE html><html><body></body></html>');
+    (global as any).window = dom.window;
+    (global as any).document = dom.window.document;
 
     // Keyringをリセット
     keyring.reset();
@@ -117,5 +127,60 @@ describe('GeoloniaMap constructor options', () => {
     assert.equal(options.pitch, 45, 'constructor params should add new properties');
     assert.equal(options.center[0], 139.767125, 'non-overridden attributes should be preserved');
     assert.equal(options.center[1], 35.681236, 'non-overridden attributes should be preserved');
+  });
+
+  it('should pass marker attributes to MapLibre Marker constructor', () => {
+    dom.window.document.body.innerHTML = `
+      <div class="geolonia"
+           data-lat="35.681236"
+           data-lng="139.767125"
+           data-marker-color="#555555"
+           style="height: 400px;">Hello
+      </div>
+    `;
+
+    const container = dom.window.document.querySelector('.geolonia') as HTMLElement;
+    new GeoloniaMap({container});
+    const loadCallback = mockMapInstance.on.getCalls().find((call) => call.args[0] === 'load')?.args[1];
+    assert(loadCallback, 'Map load event listener should be registered');
+    loadCallback({target: mockMapInstance}); // 'load' イベントを手動でトリガー
+
+    assert(markerStub.calledOnce, 'Marker constructor should be called once');
+    const [markerOptions] = markerStub.getCall(0).args;
+    assert.deepEqual(markerOptions, {color: '#555555'}, 'Marker options should include color from data-marker-color');
+    const markerInstance = markerStub.getCall(0).returnValue;
+    assert(markerInstance.setLngLat.calledWith([139.767125, 35.681236]), 'Marker position should be set correctly');
+
+    const popupInstance = maplibregl.Popup.getCall(0).returnValue;
+    assert(popupInstance.setHTML.calledWith('Hello'), 'Popup HTML should be set from div content');
+    assert(markerInstance.setPopup.calledWith(popupInstance), 'Popup should be associated with Marker');
+  });
+
+  it('should pass custom marker element', () => {
+    dom.window.document.body.innerHTML = `
+      <div>
+        <div id="custom-marker">><img src="./icon.png" alt=""></div>
+        <div class="geolonia"
+           data-lat="35.681236"
+           data-lng="139.767125"
+           data-custom-marker="#custom-marker"
+           data-custom-marker-offset="0, -25"
+           style="height: 400px;">
+           <h3>Hello World!</h3>
+        </div>
+      </div>
+    `;
+
+    const container = dom.window.document.querySelector('.geolonia') as HTMLElement;
+    new GeoloniaMap({container});
+    const loadCallback = mockMapInstance.on.getCalls().find((call) => call.args[0] === 'load')?.args[1];
+    assert(loadCallback, 'Map load event listener should be registered');
+    loadCallback({target: mockMapInstance}); // 'load' イベントを手動でトリガー
+
+    assert(markerStub.calledOnce, 'Marker constructor should be called once');
+    const [markerOptions] = markerStub.getCall(0).args;
+    assert(markerOptions['element'], 'Marker options should include custom marker element');
+    assert(markerOptions['element'].id === 'custom-marker');
+    assert.deepEqual(markerOptions['offset'], [0, -25], 'Marker options should include offset from data-custom-marker-offset');
   });
 });
